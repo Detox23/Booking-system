e482688f4a3ba7c9313721d1b60bc54a2d529510
Patrycja Suprun <patrycja@snapp.dk>
2019-10-21 21:54:37 +0200

    dodanie swagger

--- TeamTegn_Booking_API/src/main/java/API/Controllers/MainController.java
+++ TeamTegn_Booking_API/src/main/java/API/Controllers/MainController.java
@@ -3,22 +3,22 @@ import Objects.Factory.PersonFactory;
 import Objects.Factory.Account;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/main")
 @RestController
-public class MainController {
+public class MainController extends BaseController {
     private PersonFactory pf = new PersonFactory();
     private Account acc1 = (Account) pf.getPerson("Account", "Adam", 23);
 
-    @RequestMapping("/")
     public String index(){
         return "Hello";
     }
 
-    @RequestMapping(value="/API/account", method = RequestMethod.GET)
+    @RequestMapping(value="/account", method = RequestMethod.GET)
     public String see_account(){
         return acc1.getName();
     }
 
-    @RequestMapping(value= "/API/{fooId}/{name}", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value= "/{fooId}/{name}", method = {RequestMethod.GET, RequestMethod.POST})
     public String changePerson(@PathVariable String fooId, @PathVariable String name){
         acc1.setName(name);
         return acc1.getName();
