be12582185436217a67df92cb60184ce41ba7bf0
Patrycja Suprun <patrycja@snapp.dk>
2019-10-21 21:56:04 +0200

    dodanie swagger

---
+++ TeamTegn_Booking_API/src/main/java/API/Controllers/BaseController.java
@@ -0,0 +1,9 @@
+package API.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public abstract class BaseController {
+    //here we can make chceck if user is authenticated, i will try to make policies
+}
---
+++ TeamTegn_Booking_API/src/main/java/API/Repositories/BaseRepository.java
@@ -0,0 +1,5 @@
+package API.Repositories;
+
+public class BaseRepository {
+    //pisza zeby nie robic base repository i  base service robic tylko base controller
+}
