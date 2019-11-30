65c9868ebff1da612cfb6642d140329d73b1800f
Jakub Kida <1067677@ucn.dk>
2019-11-30 16:24:36 +0100

    Assignment Importance implementation

--- TeamTegn_Booking_API/src/main/java/API/Repository/Assignment/AssignmentImportanceDAO.java
+++ TeamTegn_Booking_API/src/main/java/API/Repository/Assignment/AssignmentImportanceDAO.java
@@ -1,7 +1,17 @@
 package API.Repository.Assignment;
 
+import API.Database_Entities.AssignmentImportanceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface AssignmentImportanceDAO {
+public interface AssignmentImportanceDAO extends JpaRepository<AssignmentImportanceEntity, Integer>, AssignmentImportanceDAOCustom {
+    List<AssignmentImportanceEntity> findAllByDeletedIsFalse();
+
+    Optional<AssignmentImportanceEntity> findByIdAndDeletedIsFalse(int id);
+
+    int countAllByImportanceNameIs(String importanceName);
 }
--- TeamTegn_Booking_API/src/main/java/API/Repository/Assignment/AssignmentImportanceDAOImpl.java
+++ TeamTegn_Booking_API/src/main/java/API/Repository/Assignment/AssignmentImportanceDAOImpl.java
@@ -2,12 +2,17 @@ package API.Repository.Assignment;
 
 import API.Configurations.Patcher.PatcherHandler;
 import API.Database_Entities.AssignmentImportanceEntity;
+import API.Exceptions.DuplicateException;
+import API.Exceptions.NotFoundException;
+import API.Exceptions.UpdatePatchException;
 import Shared.ToReturn.AssignmentImportanceDto;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import java.beans.IntrospectionException;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class AssignmentImportanceDAOImpl implements AssignmentImportanceDAOCustom {
@@ -34,26 +39,69 @@ public class AssignmentImportanceDAOImpl implements AssignmentImportanceDAOCusto
 
     @Override
     public AssignmentImportanceDto addAssignmentImportance(AssignmentImportanceEntity assignmentImportance) {
-        return null;
+        try {
+            int count = assignmentImportanceDAO.countAllByImportanceNameIs(assignmentImportance.getImportanceName());
+            if (count > 0) {
+                throw new DuplicateException(String.format("The status with name: %s already exists", assignmentImportance.getImportanceName()));
+            }
+            AssignmentImportanceEntity saved = assignmentImportanceDAO.save(assignmentImportance);
+            return modelMapper.map(saved, AssignmentImportanceDto.class);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     @Override
     public boolean deleteAssignmentImportance(int id) {
-        return false;
+        try {
+            AssignmentImportanceEntity found = findIfExistsAndReturn(id);
+            found.setDeleted(true);
+            AssignmentImportanceEntity deletionResult = assignmentImportanceDAO.save(found);
+            return deletionResult.isDeleted();
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     @Override
     public List<AssignmentImportanceDto> listAssignmentImportance() {
-        return null;
+        try {
+            return modelMapper.map(assignmentImportanceDAO.findAllByDeletedIsFalse(), new TypeToken<List<AssignmentImportanceDto>>() {
+            }.getType());
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     @Override
     public AssignmentImportanceDto findAssignmentImportance(int id) {
-        return null;
+        try {
+            AssignmentImportanceEntity found = findIfExistsAndReturn(id);
+            return modelMapper.map(found, AssignmentImportanceDto.class);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     @Override
     public AssignmentImportanceDto updateAssignmentImportance(AssignmentImportanceEntity assignmentImportance) {
-        return null;
+        try {
+            AssignmentImportanceEntity found = findIfExistsAndReturn(assignmentImportance.getId());
+            patcherHandler.fillNotNullFields(found, assignmentImportance);
+            AssignmentImportanceEntity result = assignmentImportanceDAO.save(found);
+            return modelMapper.map(result, AssignmentImportanceDto.class);
+        } catch (IntrospectionException introspectionException) {
+            throw new UpdatePatchException("There was an error while updating an importance [PATCHING].");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private AssignmentImportanceEntity findIfExistsAndReturn(int id) {
+        Optional<AssignmentImportanceEntity> found = assignmentImportanceDAO.findByIdAndDeletedIsFalse(id);
+        if (!found.isPresent()) {
+            throw new NotFoundException(String.format("Importance with id: %d was not found.", id));
+        }
+        return found.get();
     }
 }
